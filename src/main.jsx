8ff89aeb7ee4183ba2937a8a5b08ebbe73389987
SM-16 <sergio.martinez.edo@gmail.com>
2025-04-23 08:16:18 +0200

    CDN threejs

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Track from './track.jsx'
 import Ground from './Ground.jsx'
 import Car from './Car.jsx'
 import { Physics } from '@react-three/cannon'
+
 createRoot(document.getElementById('root')).render(
   <Canvas>
     <Physics broadphase='SAP' gravity={[0, -2.6, 0]}>
